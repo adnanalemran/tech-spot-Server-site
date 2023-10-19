0f3fc1f325e68591e0102d943295052ae8284bc7
adnanalemran <adnanalemranontor@gmail.com>
2023-10-19 13:10:28 +0600

    cart data store and show

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-const uri =
-    "mongodb+srv://TechSpot:tN3JWkfpVxZLyU6D@cluster0.fhwdeyh.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://TechSpot:tN3JWkfpVxZLyU6D@cluster0.fhwdeyh.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -20,7 +19,10 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("productDB").collection("products");
+        const userCollection = client.db("productDB").collection("user");
+        const productCartCollection = client.db("productDB").collection("cart");
 
+        //product api
         app.post("/product", async (req, res) => {
             const product = req.body;
             try {
@@ -39,7 +41,6 @@ async function run() {
             const result = await productCollection.find().toArray();
             res.send(result);
         });
-
 
 
         app.get("/product/:id", async (req, res) => {
@@ -65,37 +66,34 @@ async function run() {
         app.put("/product/update/:id", async (req, res) => {
             const id = req.params.id;
             const data = req.body;
-          
+
             console.log("id", id, data);
-          
+
             const filter = { _id: new ObjectId(id) };
             const updatedProduct = {
-              $set: {
-                name: data.name,
-                image: data.image,
-                brandName: data.brandName,
-                type: data.type,
-                price: data.price,
-                shortDescription: data.shortDescription,
-                rating: data.rating,
-              },
+                $set: {
+                    name: data.name,
+                    image: data.image,
+                    brandName: data.brandName,
+                    type: data.type,
+                    price: data.price,
+                    shortDescription: data.shortDescription,
+                    rating: data.rating,
+                },
             };
-          
+
             try {
-              const result = await productCollection.updateOne(filter, updatedProduct);
-              if (result.modifiedCount === 1) {
-                res.json({ message: "Product updated successfully" });
-              } else {
-                res.status(404).json({ error: "Product not found" });
-              }
+                const result = await productCollection.updateOne(filter, updatedProduct);
+                if (result.modifiedCount === 1) {
+                    res.json({ message: "Product updated successfully" });
+                } else {
+                    res.status(404).json({ error: "Product not found" });
+                }
             } catch (error) {
-              console.error("Error updating product:", error);
-              res.status(500).json({ error: "Internal server error" });
+                console.error("Error updating product:", error);
+                res.status(500).json({ error: "Internal server error" });
             }
-          });
-          
-
-
+        });
 
         app.delete("/product/:id", async (req, res) => {
             const id = req.params.id;
@@ -105,6 +103,69 @@ async function run() {
             };
             const result = await productCollection.deleteOne(query);
             console.log(result);
+            res.send(result);
+        });
+
+        //User api
+        app.post("/user", async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            try {
+                const result = await userCollection.insertOne(user);
+                console.log("Inserted document with _id: " + result.insertedId);
+                res.status(201).json({ message: "User added successfully" });
+            } catch (error) {
+                console.error(error);
+                res
+                    .status(500)
+                    .json({ error: "Failed to insert data into the database" });
+            }
+        });
+
+
+        app.get("/user", async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+        // single user 
+        app.get("/user/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
+
+
+        //card user data 
+
+        app.post("/cart", async (req, res) => {
+            const product = req.body;
+            console.log(product);
+            try {
+                const result = await productCartCollection.insertOne(product);
+                console.log("Inserted document with _id: " + result.insertedId);
+                res.status(201).json({ message: "Product added successfully" });
+            } catch (error) {
+                console.error(error);
+                res
+                    .status(500)
+                    .json({ error: "Failed to insert data into the database" });
+            }
+        });
+
+        app.get("/cart/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await productCartCollection.findOne(query);
+            res.send(result);
+        });
+        
+        app.get("/cart", async (req, res) => {
+            const result = await productCartCollection.find().toArray();
             res.send(result);
         });
 
